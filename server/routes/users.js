b3a9a74fec399ab625f968c2d3c919e887badaa5
john <jrborges52@gmail.com>
2022-09-28 21:30:04 -0700

    implement twilio

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,37 +1,27 @@
 const router = require('express').Router();
 
-const users = ['Bob', 'Alex', 'Will', 'Tristan'];
+// all routes will go here 
+router.post('/sendsms', (req, res) => {
 
-module.exports = (db) => {
-  // all routes will go here 
-  router.post('/sendsms', (req, res) => {
+  const message = req.body.finalMessage;
+  const telephone = req.body.telephone;
 
-    const sid = "ACafe9c39df5b4f4d142755262a0691bce";
-    const authToken = "f8afcc4985deb5297131dafa922cdd87";
-    const twilio = require('twilio')(sid, authToken);
+  console.log(message)
+  console.log(telephone)
 
-    twilio.messages.create({
-      from: "+17179876702",
-      to: req.query.telephone,
-      body: req.query.message
-    })
-      .then(res => res.send("message sent!")
-      })
-    .catch(err => {
-      console.log(err)
-    })
+  const sid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN;
+  const twilio = require('twilio')(sid, authToken);
 
+  twilio.messages.create({
+    from: process.env.PHONE_NUMBER,
+    to: telephone,
+    body: message
+  })
 
+  res.send(message)
 
-
-
-
-
-
-  res.json(users);
 });
 
-return router;
-}
 
 module.exports = router;
